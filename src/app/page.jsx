e84a5e72a544e6fb0fa8e7e9e946be07e9b5e553
Abrahan W. Guatemal Alba <aguatemala@siccec.com.ec>
2024-03-20 12:22:38 -0500

    primer comit

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h3>Hola desde la app react</h3>
+    </div>
+  );
+}
